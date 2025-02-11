7f8e8300f7109b66d30199e964561a84826297aa
Judiazj <judiazj@unal.edu.co>
2025-02-10 20:45:58 -0500

    Configuracion documentacion agregada

--- back-recetapp/src/main.ts
+++ back-recetapp/src/main.ts
@@ -1,15 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { envs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('Recetapp')
+    .setDescription('Esta es una api de recetas saludables')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, documentFactory);
+
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
 
-  await app.listen(3000);
+  await app.listen(envs.port);
 }
 bootstrap();
--- back-recetapp/src/users/dto/create-user.dto.ts
+++ back-recetapp/src/users/dto/create-user.dto.ts
@@ -1,17 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 
 
 export class CreateUserDto {
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   password: string;
