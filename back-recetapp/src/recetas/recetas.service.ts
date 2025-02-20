139d7c695b7e2b5a552628f84ee5bdf97d097658
Judiazj <judiazj@unal.edu.co>
2025-02-19 23:44:48 -0500

    relacion preferencias centralizada

--- back-recetapp/src/recetas/recetas.service.ts
+++ back-recetapp/src/recetas/recetas.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { lastValueFrom } from 'rxjs';
+import { Preferences } from '../users/enums/preferences.enum';
 
 @Injectable()
 export class RecetasService {
   private readonly urlApi = 'https://67ad62e73f5a4e1477dd8388.mockapi.io/api/v1/receta';
 
   private readonly relacionPreferencias = {
-    deportista: ['Alta en proteína', 'Bajas en calorías', 'Fáciles y rápidas'],
-    ganar_musculo: ['Alta en proteína'],
-    familiar: ['Comida familiar y reconfortante'],
-    perder_peso: ['Bajas en calorías', 'Veganas'],
-    vegano: ['Veganas'],
-    ajetreado: ['Fáciles y rápidas', 'Comida familiar y reconfortante'],
+    [Preferences.deportista]: ['Alta en proteína', 'Bajas en calorías', 'Fáciles y rápidas'],
+    [Preferences.ganar_musculo]: ['Alta en proteína'],
+    [Preferences.familiar]: ['Comida familiar y reconfortante'],
+    [Preferences.perder_peso]: ['Bajas en calorías', 'Veganas'],
+    [Preferences.vegano]: ['Veganas'],
+    [Preferences.ajetreado]: ['Fáciles y rápidas', 'Comida familiar y reconfortante'],
   };
 
-  constructor(private readonly httpService: HttpService) {}
+  constructor(private readonly httpService: HttpService) { }
 
   async filtrarRecetas(preferencia: string) {
     const categorias = this.relacionPreferencias[preferencia] || [];
@@ -27,7 +28,7 @@ export class RecetasService {
     const response = await lastValueFrom(recetas$);
 
     const recetas = response.data;
-    return recetas.filter((receta: any) => 
+    return recetas.filter((receta: any) =>
       receta.tipo.some((tipo: string) => categorias.includes(tipo))
     );
   }
